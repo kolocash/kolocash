949bb78882416d7887d06abd74c6266c0e9ab746
Charles <charles@charlesen.fr>
2024-10-16 16:45:56 +0200

    Init Kolocash hardat

---
+++ contracts/Kolocash.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+/// @custom:security-contact tech@kolo.cash
+contract Kolocash is ERC20, ERC20Burnable, Ownable {
+    uint256 public initialSupply = 450000000; //
+    // constructor(uint256 initialSupply)
+    constructor() ERC20("Kolocash", "KOLO") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply); // Mint initial supply to the owner's address
+        transferOwnership(msg.sender); // Transfer ownership to the specified initial owner
+    }
+
+    // Function to mint new tokens, accessible only by the owner
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
