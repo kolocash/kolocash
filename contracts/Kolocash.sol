eaeb71f1ae07d7f318028b1ba8de7dcd642be613
Charles <charles@charlesen.fr>
2024-10-16 16:47:33 +0200

    Update solidity version

--- contracts/Kolocash.sol
+++ contracts/Kolocash.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact tech@kolo.cash
 contract Kolocash is ERC20, ERC20Burnable, Ownable {
